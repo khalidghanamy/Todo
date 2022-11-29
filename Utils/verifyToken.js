57a3401fa492ef47b3bcce3e206dbcdd48301c36
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-11-29 23:24:14 +0200

    update

--- Utils/verifyToken.js
+++ Utils/verifyToken.js
@@ -5,32 +5,32 @@ export const verifyToken = (req, res, next) => {
     const bearerHeader = req.headers["authorization"];
     const [_, bearerToken] = bearerHeader.split(" ");
     if (!bearerToken) {
-            return res.status(401).json({status:false,msg:"Login first"});
-        }
-        
-    jwt.verify(bearerToken, process.env.JWT_SECRET,(err,user)=>{
-            if(err){
-                return res.status(401).json({status:false,msg:"Invalid token"});
-            }
-                req.user = user.user
-                return next();
-             })
-      
-
+        return res.status(401).json({ status: false, msg: "Login first" });
     }
+
+    jwt.verify(bearerToken, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(401).json({ status: false, msg: "Invalid token" });
+        }
+        req.user = user.user
+        return next();
+    })
+
+
+}
 
 
 export const verifyUser = (req, res, next) => {
-    
-         verifyToken(req, res, ()=> {
-            if(req.user.id == req.params.userId){
-                next()
-            }
-            else{
-                
-                return res.status(401).json({status:false,msg:"user is not verified"});
-            }
-    
-        })
-    }
+
+    verifyToken(req, res, () => {
+        if (req.user.id == req.params.userId) {
+            next()
+        }
+        else {
+
+            return res.status(401).json({ status: false, msg: "user is not verified" });
+        }
+
+    })
+}
 
